b92cdc458efb65f11e83cda1bffb82514b5c8273
brightboost <info@brightboost.nl>
2022-10-31 18:37:59 +0100

    more jdbc

--- sakila-web/src/main/java/com/hca/servlets/sakila/ExampleJDBC.java
+++ sakila-web/src/main/java/com/hca/servlets/sakila/ExampleJDBC.java
@@ -4,34 +4,51 @@ import java.sql.*;
 
 public class ExampleJDBC {
 	public static void main(String[] args) {
+		// I did not set a pw for mysql, so leaving this out
 //		if(args.length != 2) {
 //			System.err.println("Not enough args");
 //			System.exit(1);
 //		}
 		String user = args[0];
+		// no pw
 		String password = "";// args[1];
 		
+		Connection connection = null;
+		PreparedStatement preparedStatement = null;
+		ResultSet rs = null;
+		
+		
 		try {
-			Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/sakila", user, password);
+			connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/sakila", user, password);
 			String query = "SELECT first_name, last_name FROM customer WHERE last_name LIKE ?"; 
-			PreparedStatement preparedStatement = connection.prepareStatement(query);
+			preparedStatement = connection.prepareStatement(query);
 			preparedStatement.setString(1, "Sa%");
-			ResultSet rs = preparedStatement.executeQuery();
+			rs = preparedStatement.executeQuery();
 			
 			while(rs.next()) {
 				String s1 = rs.getString("first_name");
 				String s2 = rs.getString("last_name");
 				System.out.println(s1 + " " + s2);
 			}
-			rs.close();
-			preparedStatement.close();
-			connection.close();
-			
-		
+				
 			
 		} catch (SQLException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
+		} finally {
+			System.out.println("almost always executed");
+			try {
+				if(rs != null) {
+					rs.close();
+				}
+				if(preparedStatement != null) {
+					preparedStatement.close();
+				}
+				if(connection != null) {
+					connection.close();	
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
 		}
 
 	}
